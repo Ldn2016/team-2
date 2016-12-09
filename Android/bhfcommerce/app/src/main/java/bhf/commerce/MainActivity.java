de164fed7836f0e5db0f667ae4f5cdeaab8c9fd3
Willy Halim Dinata <whdinata@gmail.com>
2016-12-09 21:03:51 +0000

    AddDetailActivity - added permission request

--- Android/bhfcommerce/app/src/main/java/bhf/commerce/MainActivity.java
+++ Android/bhfcommerce/app/src/main/java/bhf/commerce/MainActivity.java
@@ -1,5 +1,9 @@
 package bhf.commerce;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
