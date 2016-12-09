72d55f6e47840dd1a3886e3d262fba90add3e3b9
Willy Halim Dinata <whdinata@gmail.com>
2016-12-09 21:28:03 +0000

    AddDetailActivity - added camera feature

--- Android/bhfcommerce/app/src/main/java/bhf/commerce/ui/activities/AddItemActivity.java
+++ Android/bhfcommerce/app/src/main/java/bhf/commerce/ui/activities/AddItemActivity.java
@@ -1,22 +1,89 @@
 package bhf.commerce.ui.activities;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+import java.io.File;
 
 import bhf.commerce.R;
+import bhf.commerce.utils.ImageUtil;
 
-public class AddItemActivity extends AppCompatActivity {
+public class AddItemActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final int OPEN_CAMERA = 1;
+    private static final int OPEN_GALLERY = 2;
+    private ImageView imageChooser;
+    private EditText etName;
+    private EditText etDescription;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_item);
-
+        initView();
         askPermission();
+    }
+
+    private void initView(){
+        setContentView(R.layout.activity_add_item);
+        imageChooser = (ImageView) findViewById(R.id.image);
+        etName = (EditText) findViewById(R.id.name);
+        etDescription = (EditText) findViewById(R.id.description);
+
+        imageChooser.setOnClickListener(this);
+        findViewById(R.id.submit).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.image:
+                openCamera();
+                break;
+            case R.id.submit:
+                break;
+        }
+    }
+
+    private void openCamera(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(), "Pic.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, OPEN_CAMERA);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Uri selectedImage;
+
+        if(requestCode == OPEN_CAMERA){
+            selectedImage = imageUri;
+        } else{
+            selectedImage = data.getData();
+        }
+
+        if(selectedImage != null)
+            getContentResolver().notifyChange(selectedImage, null);
+
+        Bitmap bitmap = ImageUtil.rotateBitmap(getContentResolver(), selectedImage);
+
+        imageChooser.setImageBitmap(bitmap);
     }
 
     private void askPermission(){
@@ -24,26 +91,15 @@ public class AddItemActivity extends AppCompatActivity {
                 Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) {
 
-            // Should we show an explanation?
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.READ_EXTERNAL_STORAGE)) {
 
-                // Show an expanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
             } else {
-
-                // No explanation needed, we can request the permission.
 
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,
                                 Manifest.permission.CAMERA},
                         0);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
             }
         }
     }
---
+++ Android/bhfcommerce/app/src/main/java/bhf/commerce/utils/ImageUtil.java
@@ -0,0 +1,66 @@
+package bhf.commerce.utils;
+
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+/**
+ * Created by whdinata on 9/28/16.
+ */
+public class ImageUtil {
+    public static Bitmap rotateBitmap(ContentResolver cr, Uri uri){
+        try {
+            Bitmap bitmap = MediaStore.Images.Media
+                    .getBitmap(cr, uri);
+            Bitmap newImage = resizeImage(bitmap);
+            Bitmap rotatedBitmap = newImage;
+
+            ExifInterface ei = new ExifInterface(uri.getPath());
+            int orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+
+            switch(orientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    System.out.println("Rotate: 90");
+                    rotatedBitmap = rotateImage(newImage, 90);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    System.out.println("Rotate: 180");
+                    rotatedBitmap = rotateImage(newImage, 180);
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    System.out.println("Rotate: 270");
+                    break;
+                default:
+                    System.out.println("Rotate: 0");
+            }
+
+            return rotatedBitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static Bitmap resizeImage(Bitmap bitmap) {
+        int newWidth = (int) (bitmap.getWidth() * 0.9);
+        int newHeight = (int) (bitmap.getHeight() * 0.9);
+        return Bitmap.createScaledBitmap(bitmap,
+                newWidth,
+                newHeight,
+                false);
+    }
+
+    private static Bitmap rotateImage(Bitmap source, float angle) {
+        Bitmap retVal;
+
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        retVal = Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+
+        return retVal;
+    }
+}
